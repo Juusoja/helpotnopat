7b73d3d039077e5cffbf388b7dc14dc1ba4d3dc1
Juuso Jahnukainen <juuso.jahnukainen@aalto.fi>
2018-11-10 21:14:51 +0200

    AI rajapintaesimerkki valmis

--- examples/ai_module.py
+++ examples/ai_module.py
@@ -1,4 +1,5 @@
 import json
+""" YOUR CODE HERE """
 
 class Ai(object):
 
@@ -13,12 +14,27 @@ class Ai(object):
         self.name = name
 	self.game_state = ""
 
-    def set_info(self, n):
+    def set_info(self, state):
         """Use this method to give the game state to the AI
         """
-        return [random.randint(1,6) for i in range(1,n+1)]
+	try :
+            self.game_state = json.loads(state) 
+	    return 0
+	except ValueError:
+	    return 1
 
     def get_action(self, n):
         """Use this method to get action base on the given game info.
         """
-        return [random.randint(1,6) for i in range(1,n+1)]
+
+	""" Options to return: 
+		1) Raise: return list with first index denoting the number
+			of dices, the second their pips.
+			Example: [2,4] -> "There are atleast two fours"
+		2) Doubt: return [0]
+		3) Exact: return [-1]
+
+
+	""" YOUR CODE HERE """
+
+        return [-1]
