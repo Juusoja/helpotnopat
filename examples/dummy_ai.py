25af1db1dcbe47462dea8ec83a41fe89deba7d52
Juuso Jahnukainen <juuso.jahnukainen@aalto.fi>
2018-11-10 23:14:56 +0200

    Dummy AI lisätty testejä varten

--- examples/dummy_ai.py
+++ examples/dummy_ai.py
@@ -9,26 +9,27 @@ class Ai(object):
     """
 
     def __init__(self, name):
+
         """Return an AI object whose name is *name*.
 	"""
+
         self.name = name
 	self.game_state = ""
 
-    def set_info(self, state):
-        """Use this method to give the game state to the AI
-        """
+    def get_action(self, state):
+
 	try :
             self.game_state = json.loads(state)
 	    return 0
 	except ValueError:
 	    return 1
 
-	self.hand =
-	self.previous_call =
-	self.players = 
-	
+	self.hand = self.gamestate['hand']
+	self.previous_call = self.gamestate['previous_call']
+	self.players = len(self.gamestate['player_status'])
+	self.dice_sum = self.dice_count()
+	self.mininc = self.calculate_minimum_increment()
 
-    def get_action(self, n):
         """Use this method to get action base on the given game info.
         """
 
@@ -40,16 +41,39 @@ class Ai(object):
 		3) Exact: return 1
 	"""
 
-	
+	if (self.call_falsehood_probability() > 0.6):
+	    return 0
 
-	if (previous action)
+        return self.compressed(self.mininc)
 
-        return 0
-
-    def get_dice_count(self):
+    def dice_count(self):
 
 	sum = 0
-	for i in range(len(self.gamestate['player_status'])):
+	for i in range(self.players):
 	    sum += self.gamestate['player_status'][i]['dice']
 	return sum
 
+    def call_falsehood_probability(self):
+
+	return self.previous_call[0]*self.previous_call[1]/face_probability()*1.0
+
+    def face_probabililty(self):
+
+	return self.dice_sum/6.0
+
+    def compressed(self, tobecomp):
+
+	return 10*tobecomp[0]+tobecomp[1]
+
+    def calculate_min_increment(self):
+
+	self.placeholder = [0,0]
+
+	if (self.previous_call[1] == 6):
+	    self.placeholder[0] = self.previous_call[0] + 1
+	    self.placeholder[1] = 1
+	else:
+	    self.placeholder[0] = self.previous_call[0]
+	    self.placeholder[1] = self.previous_call[1] + 1
+
+	return self.placeholder
